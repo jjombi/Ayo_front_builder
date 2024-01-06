ba51e5176ab3a8f9a36b16766b01c54aa06c03b6
jjjombi <sis01066745950@gmail.com>
2024-01-06 20:29:28 +0900

    svg 사용할때 html작성 법으로 써서 생긴 에러 수정

--- front/src/App/Main2.js
+++ front/src/App/Main2.js
@@ -73,7 +73,7 @@ const Main2 = () => {
                 <button className="all_btn main2_header_btn">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
                     <circle cx="11" cy="11" r="6" stroke="#222222"/>
-                    <path d="M20 20L17 17" stroke="#222222" stroke-linecap="round"/>
+                    <path d="M20 20L17 17" stroke="#222222" strokeLinecap="round"/>
                     </svg>
                 </button>
             </header>
