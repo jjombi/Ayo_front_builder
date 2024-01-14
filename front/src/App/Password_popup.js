a505a68ed92ac994fac7ccc7f8bc373d4998a129
jjjombi <sis01066745950@gmail.com>
2024-01-14 22:21:57 +0900

    Revert "main conent 글시 가려지던거 수정,"
    This reverts commit 991ec5ab009f3cfd65ff895d6592a5123596e7dd.

--- front/src/App/Password_popup.js
+++ front/src/App/Password_popup.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useRef } from 'react';
 import {useNavigate} from 'react-router-dom';
 
-const Password_popup = ({setPopup_state , uuid, roomName, title, publicAccess, type}) => {
+const Password_popup = ({ uuid, roomName, title, publicAccess, type }) => {
     const input_password_ref = useRef();
     const navigate = useNavigate();
 
@@ -28,13 +28,10 @@ const Password_popup = ({setPopup_state , uuid, roomName, title, publicAccess, t
             }
         })
     }
-    const change_popup_state = () => {
-        setPopup_state(popup_state => !popup_state);
-    }
+
     return(
         <div className='password_popup_root'>
             <section>
-                <button onClick={change_popup_state} className='all_btn'>X</button>
                 <p>비밀번호</p>
                 <input type='password' ref={input_password_ref} placeholder='비밀번호를 입력해 주세요'></input>
                 <input type='button' className='all_btn' onClick={password_checker} value={'완료'}></input>
