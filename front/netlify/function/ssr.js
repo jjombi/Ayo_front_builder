c5fe6a5c8fb04d8d003222b1e026968b55852025
jjjombi <sis01066745950@gmail.com>
2024-04-01 19:43:38 +0900

    Update ssr.js

--- front/netlify/function/ssr.js
+++ front/netlify/function/ssr.js
@@ -1,14 +1,17 @@
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
+const fs = require('fs');
+const path = require('path');
 // const App = require('../../src/App/Main2'); // Import your root React component
-const App = require('../../src/App/Quezeshow/Quezeshow_main');
+// const App = require('../../src/App/Quezeshow/Quezeshow_main');
 exports.handler = async (event, context) => {
   try {
     const { path } = event;
     // const parameter = extractParameterFromPath(path);
     console.log('path',path,'parameter',);
+    const html = fs.readFileSync(path.resolve(__dirname, "../../build/index.html"), "utf-8");
     // Render your React component to HTML
-    const html = ReactDOMServer.renderToString(<App />);
+    // const html = ReactDOMServer.renderToString(<App />);
 
     // Return the HTML content as the response
     return {
