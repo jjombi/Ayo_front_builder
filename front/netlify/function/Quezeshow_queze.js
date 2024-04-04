0f7c48e10a70fffcdb731ae1d99518adae3bf03c
jjjombi <sis01066745950@gmail.com>
2024-04-04 10:56:05 +0900

    seo 페이지별 meta 변경

--- front/netlify/function/Quezeshow_queze.js
+++ front/netlify/function/Quezeshow_queze.js
@@ -12,17 +12,17 @@ exports.handler =  async (event, context) => {
             <html lang="kr">
             <head>
                 <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" content="ayo 퀴즈쇼 문제 맞추기 웃기고 재미있는 게임">
-                <title>ayo 퀴즈쇼 문제 맞추기 웃기고 재미있는 게임 즐기기/title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" content="Super easy한 퀴즈쇼">
+                <title>Super easy한 퀴즈쇼</title>
                 <link rel="icon" sizes="16x16" type="image/x-icon" href="/favicon.ico?">
                 <link rel="apple-touch-icon" sizes="16x16" href="/favicon.ico?">
                 <meta name="referrer" content="no-referrer-when-downgrade" />
                 <meta name="google-adsense-account" content="ca-pub-7471498859383871">
-                <meta name="title" content="ayo 퀴즈쇼 문제 맞추기 웃기고 재미있는 게임 즐기기">
-                <meta name="description" content="퀴즈쇼 이상형월드컵 문제 맞추기 웃기고 재미있는 게임 이어말하기 게임 신조어맞추기 게임 등을 즐기는 사이트 입니다">
+                <meta name="title" content="ayo, 이상형 월드컵과 퀴즈쇼를 즐겨보세요.">
+                <meta name="description" content="나락퀴즈쇼 아이돌퀴즈쇼 애니퀴즈쇼 상식퀴즈쇼 정치퀴즈쇼 등 웃기고 재미있는 퀴즈쇼를 만나보세요">
                 <meta name=”robots” content=”index,follow” />
                 <link rel="canonical" href="https://ay0.site">
-                <meta name="keywords" content="ayo, ay0, 이상형 월드컵, 이상형월드컵, 맞추기, 마추기, 퀴즈쇼, 이어말하기, 신조어 맞추기, 퀴즈" />
+                <meta name="keywords" content="ayo, ay0, 퀴즈쇼, " />
 
 
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7471498859383871"
