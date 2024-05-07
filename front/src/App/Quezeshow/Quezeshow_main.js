b010c5a2004e436ededc6ee47005f0aa850c6854
jjjombi <sis01066745950@gmail.com>
2024-05-07 09:41:54 +0900

    검색 기능 update

--- front/src/App/Quezeshow/Quezeshow_main.js
+++ front/src/App/Quezeshow/Quezeshow_main.js
@@ -6,58 +6,33 @@ import axios from "axios";
 import Adfit from "../Adfit";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+
 const Quezeshow_main= ({match}) => {
     const [content_state,setContent_state] = useState([]);
     const navigate = useNavigate();
-    const [seachParams, setSearchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useSearchParams();
     const search_value_ref = useRef(null);
-    // const space_uuid = seachParams.get('space_uuid');
-    // const space_title = seachParams.get('space_title');
-    const intro_text = seachParams.get('intro_text');
+    const search_tag = searchParams.get('tag');
     useEffect(()=>{
-        // const htmlTitle = document.querySelector("title");
-        // htmlTitle.innerHTML = '나락퀴즈쇼 당신도 나락에 갈수 있다';
-        // console.log('useeffect',space_uuid);
-        axios({// parma으로 최신순, 좋아요순 변경
-            url : process.env.REACT_APP_SERVER_URL + '/quezeshow_main',
-            method : 'GET',
-            params : { 
-                type : 'likes',
-                quezeshow_type : "&& quezeshow_type = 'vote'"
-                // space_uuid : space_uuid,
-            }
-        }).then(res=>{ //res = [{src, uuid, title}]
-            // console.log(res);
-            const content_state_ = res.data.map((e,i)=>{
-                return({
-                    src : 'data:image/jpeg;base64,'+e.img,
-                    uuid : e.uuid, //    space  queze
-                    // uuid2 : e.uuid2, //  queze  null
-                    title : e.title,
-                    likes : e.likes,
-                    roomnum : e.roomnum,
-                    quezeshow_type : e.quezeshow_type,
-                    explain_text : e.explain_text
-                })
-            })
-            setContent_state(content_state => [...content_state_]);
-        })
-    },[])
-    const search_axios = () => {
+        // console.log('search_tag',search_tag);
+        if(search_tag === null){
+            search_axios('');
+        }else if(search_tag === undefined){
+            search_axios('');
+        }else if(search_tag === ''){
+            search_axios('');
+        }else {
+            search_axios(search_tag);
+        }
+    },[search_tag])
+    const search_axios = (tag) => {
         axios({
             url : process.env.REACT_APP_SERVER_URL + '/search_quezeshow',
             method : 'GET',
             params : {
-                value : search_value_ref.current.value
+                value : tag
             }
-            // headers : {
-            //     'Content-Type' : 'application/json'
-            // },
-            // data : {
-            //     value : search_value_ref.current.value
-            // }
         }).then((res)=>{
-            // console.log(res);
             const content_state_ = res.data.map((e,i)=>{
                 return({
                     src : 'data:image/jpeg;base64,'+e.img,
@@ -73,13 +48,14 @@ const Quezeshow_main= ({match}) => {
         })
     }
     const search_enter = (e) => {
-        // console.log('seach 중',search_value_ref.current.value,e.target.id);
         if(e.key === 'Enter'){
-            search_axios();
+            searchParams.set('tag',search_value_ref.current.value);
+            setSearchParams(searchParams);
         }
     }
     const search_btn_enter = () => {
-        search_axios();
+        searchParams.set('tag',search_value_ref.current.value);
+        setSearchParams(searchParams);
     }
     return(
         <div className="quezeshow_main_root">
